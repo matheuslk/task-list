62b5f6c5ef6d939b7d3b5740da0d892d3352b732
Joao Loukachak <jv.loukachak@gmail.com>
2023-10-16 08:01:38 -0300

    chore: setup ant design with less

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { MAIN_ROUTES } from './app/routes/main.routes';
+import { NZ_I18N, pt_BR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
 
-import { AppModule } from './app/app.module';
+registerLocaleData(pt);
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(MAIN_ROUTES),
+    { provide: NZ_I18N, useValue: pt_BR },
+  ],
+});
